c93f3822f539fc311f5269c291e677954b825aad
Amir Blum <amir@aspecto.io>
2022-02-28 13:31:32 +0200

    local sampler ratio

--- src/tracing.ts
+++ src/tracing.ts
@@ -2,19 +2,34 @@ import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { OTLPTraceExporter } from '@opentelemetry/exporter-otlp-proto';
 import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { SemanticAttributes, SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import { ExpressInstrumentation } from "opentelemetry-instrumentation-express";
 import { MongooseInstrumentation } from "opentelemetry-instrumentation-mongoose";
 
-// import { getEnv } from "@opentelemetry/core"
-// import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
-// // diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-// diag.setLogger(new DiagConsoleLogger(), getEnv().OTEL_LOG_LEVEL);
+import { getEnv, ParentBasedSampler, TraceIdRatioBasedSampler } from "@opentelemetry/core"
+import { Context, diag, DiagConsoleLogger, Link, Sampler, SamplingDecision, SamplingResult, Span, SpanAttributes, SpanKind, TraceFlags } from "@opentelemetry/api";
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+diag.setLogger(new DiagConsoleLogger(), getEnv().OTEL_LOG_LEVEL);
 
+// Custom sampler that reduce noise on '/users' endpoint
+const ratioSampler: Sampler = new TraceIdRatioBasedSampler(0.001);
+const myCustomSampler: Sampler = {
+    shouldSample: function (context: Context, traceId: string, spanName: string, spanKind: SpanKind, attributes: SpanAttributes, links: Link[]): SamplingResult {
+        if(attributes[SemanticAttributes.HTTP_TARGET] === '/users') {
+            return ratioSampler.shouldSample(context, traceId, spanName, spanKind, attributes, links);
+        }
+        return {
+            decision: SamplingDecision.RECORD_AND_SAMPLED
+        }
+    }
+}
+
+// register provider with sampler
 const provider = new NodeTracerProvider({ 
-    resource: new Resource({[SemanticResourceAttributes.SERVICE_NAME]: 'otel-troubleshooting'})
+    resource: new Resource({[SemanticResourceAttributes.SERVICE_NAME]: 'otel-troubleshooting'}),
+    sampler: new ParentBasedSampler({ root: myCustomSampler }),
 });
 const exporter = new OTLPTraceExporter({ url: 'http://localhost:4318/v1/traces'});
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
